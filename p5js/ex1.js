2533d1218680b9d9a3e3f1b718855b2f7eeee2c8
joaom95 <45365912+joaom95@users.noreply.github.com>
2020-06-11 21:54:31 +0100

    tentativa2

--- p5js/ex1.js
+++ p5js/ex1.js
@@ -1,11 +1,5 @@
 //Variáveis tipo let nomeDaVar;
-
-
-
-
-
 let numDiv
-
 
 function setup() { //código que só corre uma vez
 
@@ -23,7 +17,6 @@ function setup() { //código que só corre uma vez
 function draw() { //código que corre em loop
   background(50);
 
-
    posX = mouseX
    posY = mouseY
 
@@ -31,28 +24,26 @@ function draw() { //código que corre em loop
    for ( i=0; i < numDiv-1; i ++){
      for (j=0; j < numDiv-1; j++){
        avatar ((i+1)*width/numDiv, (j+1)*height/numDiv,15); } }
-
  }
+
    function avatar (posX,posY,diam) {
-     fill (255)
-       ellipse (posX-diam/5, posY-diam/1.6,diam/0.9);
 
-       fill (100)
-       ellipse (posX-diam/2,posY-diam/1.1, diam/2);
-       ellipse (posX-diam/4,posY-diam/1, diam/2.5);
-       ellipse (posX-diam/4,posY-diam/1.3, diam/2);
-       ellipse (posX-diam/1,posY-diam/1, diam/4);
-       ellipse (posX-diam/1,posY-diam/0.4, diam/6);
-       ellipse (posX-diam/1,posY-diam/0.3,diam/6);
-       ellipse (posX-diam/1.5,posY-diam/0.3,diam/6);
+  fill (255)
+  ellipse (posX-diam/5, posY-diam/1.6,diam/0.9);
 
+  fill (100)
+  ellipse (posX-diam/2,posY-diam/1.1, diam/2);
+  ellipse (posX-diam/4,posY-diam/1, diam/2.5);
+  ellipse (posX-diam/4,posY-diam/1.3, diam/2);
+  ellipse (posX-diam/1,posY-diam/1, diam/4);
+  ellipse (posX-diam/1,posY-diam/0.3,diam/6);
+  ellipse (posX-diam/1.5,posY-diam/0.3,diam/6);
 
-       fill(100)
-       ellipse(posX,posY,diam);
+  fill(100)
+  ellipse(posX,posY,diam);
 
-       fill (255)
-       ellipse (posX-diam/4,posY-diam/10, diam/4);
+  fill (255)
+  ellipse (posX+diam/7,posY-diam/15, diam/2);
+  ellipse (posX-diam/5,posY+diam/15, diam/2.5);
 
-       fill (255)
-       ellipse (posX+diam/4, posY-diam/10,diam/4);
-   }
+}
